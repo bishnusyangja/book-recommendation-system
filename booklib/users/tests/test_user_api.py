46a98cdfd608cd3fd66b8b44fbcda834785287d2
Bishnu Bhattarai <bishnusyangja@gmail.com>
2024-09-17 17:15:00 +0545

    user api test done

--- booklib/users/tests/test_user_api.py
+++ booklib/users/tests/test_user_api.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from rest_framework.test import APIClient
 from users.models import User
+from model_bakery import baker
 
 
 class RegistrationAPITestCase(TestCase):
@@ -13,12 +14,26 @@ class RegistrationAPITestCase(TestCase):
         self.assertEqual(resp.status_code, 400)
 
     def test_user_registration_with_valid_data(self):
-        pass
-        # data  = {}
-        # check is_active
+        passwd = 'Newis#$new234'
+        data  = {'first_name': 'Ramesh Raj', 'last_name': 'Bhattarai', 'email': 'pqrs@gmail.com',
+                 'password': passwd, 'confirm_password': passwd}
+        resp = self.client.post(self.url, data=data, content_type='application/json')
+        self.assertEqual(resp.status_code, 201)
+
+    def test_user_registration_with_passwd_doesnot_match(self):
+        passwd = 'Newis#$new234'
+        data  = {'first_name': 'Ramesh Raj', 'last_name': 'Bhattarai', 'email': 'pqrs@gmail.com',
+                 'password': passwd, 'confirm_password': 'jptramsiyaram'}
+        resp = self.client.post(self.url, data=data, content_type='application/json')
+        self.assertEqual(resp.status_code, 400)
+        self.assertIn('password', resp.json().keys())
 
     def test_duplicate_email_registration(self):
-        pass
-        # email = ''
-        # baker.make(User, email=email)
-        # data = {'email': email}
+        passwd = 'Newis#$new234'
+        email = 'abcxyz@gmail.com'
+        baker.make(User, email=email)
+        data = {'first_name': 'Santosh', 'last_name': 'Sharma', 'email': email,
+                'password': passwd, 'confirm_password': passwd}
+        resp = self.client.post(self.url, data=data, content_type='application/json')
+        self.assertEqual(resp.status_code, 400)
+        self.assertIn('email', resp.json().keys())
